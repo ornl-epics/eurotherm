56d849044bda9b75293284085727ef3bcc62afa7
Matthew Pearson <pearsonmr@ornl.gov>
2020-05-14 18:05:39 -0400

    New example IOC to test out absolute addressing and reading float32 data types.

---
+++ examples/nanodac_absolute/iocBoot/iocnanodac/st.cmd
@@ -0,0 +1,35 @@
+#!../../bin/linux-x86_64/nanodac
+
+## You may have to change nanodac to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/nanodac.dbd"
+nanodac_registerRecordDeviceDriver pdbbase
+
+####################################################
+# TCP to Eurotherm
+
+epicsEnvSet("IP_ADDR","192.168.200.177:502")
+
+####################################################
+
+####################################################################
+# Set up modbus TCP support for Eurotherm
+
+epicsEnvSet("NI","1")
+
+< $(EUROTHERM)/nd.st.cmd.main
+
+####################################################################
+
+## Load record instances
+dbLoadRecords "db/nanodac.db"
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
