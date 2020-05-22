3dbf49c1e0d2a2817ceb721faec056e9901ece9a
Matthew Pearson <pearsonmr@ornl.gov>
2020-05-22 14:37:49 -0400

    examples/nanodac_absolute: add autosave support to example IOC.

--- examples/nanodac_absolute/iocBoot/iocnanodac/st.cmd
+++ examples/nanodac_absolute/iocBoot/iocnanodac/st.cmd
@@ -30,6 +30,37 @@ epicsEnvSet("NI","1")
 ## Load record instances
 dbLoadRecords "db/nanodac.db"
 
+#################################################
+# autosave
+
+epicsEnvSet IOCNAME eurotherm_absolute
+epicsEnvSet SAVE_DIR /home/controls/var/$(IOCNAME)
+
+save_restoreSet_Debug(0)
+
+### status-PV prefix, so save_restore can find its status PV's.
+save_restoreSet_status_prefix("SECAGE:CS:ND7B:")
+
+set_requestfile_path("$(SAVE_DIR)")
+set_savefile_path("$(SAVE_DIR)")
+
+save_restoreSet_NumSeqFiles(3)
+save_restoreSet_SeqPeriodInSeconds(600)
+set_pass0_restoreFile("$(IOCNAME).sav")
+set_pass0_restoreFile("$(IOCNAME)_pass0.sav")
+set_pass1_restoreFile("$(IOCNAME).sav")
+
+#################################################
+
 cd ${TOP}/iocBoot/${IOC}
 iocInit
+
+
+# Create request file and start periodic 'save'
+epicsThreadSleep(5)
+makeAutosaveFileFromDbInfo("$(SAVE_DIR)/$(IOCNAME).req", "autosaveFields")
+makeAutosaveFileFromDbInfo("$(SAVE_DIR)/$(IOCNAME)_pass0.req", "autosaveFields_pass0")
+create_monitor_set("$(IOCNAME).req", 5)
+create_monitor_set("$(IOCNAME)_pass0.req", 30)
+
 
